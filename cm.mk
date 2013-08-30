1658038d786105d4f908e1ad64aff33dab02f9a5
Hernán Castañón <hernanderes@hotmail.com>
2013-08-30 18:10:50 +0200

    Updated for compile Galaxy Mega

--- cm.mk
+++ cm.mk
@@ -2,19 +2,19 @@
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := GalaxyGrandDuos
+PRODUCT_RELEASE_NAME := GalaxyMega
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/i9082/device_i9082.mk)
+$(call inherit-product, device/samsung/i9152/device_i9152.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := i9082
-PRODUCT_NAME := cm_i9082
+PRODUCT_DEVICE := i9152
+PRODUCT_NAME := cm_i9152
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := GT-I9082
+PRODUCT_MODEL := GT-I9152
 
 #Set build fingerprint / ID / Prduct Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=baffinvj TARGET_DEVICE=baffin BUILD_FINGERPRINT=samsung/baffinvj/baffin:4.1.2/JZO54K/I9082LDCAMC2:user/release-keys PRIVATE_BUILD_DESC="baffinvj-user 4.1.2 JZO54K I9082LDCAMC2 release-keys"
